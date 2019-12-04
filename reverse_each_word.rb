71fd1a3071b169427e1a77491480151340990d0a
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-04 22:42:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(original_string)
   original_words = original_string.split(" ")
   reversed_words = original_words.collect {|word| word.reverse}
-  reversed_words.join()
+  reversed_words.join(' ')
 end
 
 p reverse_each_word("Hello there, and how are you?")
