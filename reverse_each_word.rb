d759d1c4ddd9f36af71d579456da71d635fa2eaf
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-04 22:38:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 def reverse_each_word(original_string)
   original_words = original_string.split(" ")
-  reversed_string = ""
   original_words.collect do |word| 
-      reversed_string += (word.reverse + " ")
+      word.reverse
   end
-  reversed_string[0...reversed_string.length-1]
+  original_words.
 end
 
 p reverse_each_word("Hello there, and how are you?")
