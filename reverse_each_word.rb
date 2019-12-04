f86294f846a1a9ce543f0e0702286ca8bf72197f
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-04 22:39:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(original_string)
   original_words = original_string.split(" ")
   original_words.collect do |word| 
-      word.reverse
+      word = word.reverse
   end
   original_words.join(" ")
 end
